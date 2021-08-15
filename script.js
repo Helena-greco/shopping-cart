7fe789c4251f613b7dd242813b360fde828975bb
Helena <nenafgreco@hotmail.com>
2021-08-14 22:14:30 -0300

    Requisito 2 - Finalmente

--- script.js
+++ script.js
@@ -12,24 +12,14 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+
+
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,7 +30,35 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-function fetchAPI() {
+// Função para add o item selecionado no cart__items - Requisito 2
+function fetchProductById(event) {
+  const id = event.target.parentElement.firstChild.innerText;
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => response.json())
+    .then((result) => {
+      const cartList = document.querySelector('.cart__items');
+      const cartItem = { sku: result.id, name: result.title, salePrice: result.price };
+      cartList.appendChild(createCartItemElement(cartItem));
+    });
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => button.addEventListener('click', fetchProductById));
+
+  return section;
+}
+
+// Função para criar o formato do obj e add no parent items - Requisito 1
+function fetchProductsAPI() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
     .then((products) => products.results.forEach((product) => {
@@ -55,5 +73,5 @@ function fetchAPI() {
 }
 
 window.onload = () => {
-  fetchAPI();
+  fetchProductsAPI();
  };
